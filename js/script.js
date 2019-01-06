13c99d367746b420e0d5b2f102666f77e8ff8f09
Nikita <nikikot@list.ru>
2019-01-06 20:31:36 +0300

    added sm stuff

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// start_()
+
+function start_()
+{
+	try {
+		var p = document.getElementsByTagName('iframe')
+		if (p[0].contentWindow.document.getElementsByTagName('body')[0].innerText == 'Hello world')
+			return
+	} catch(e)
+	{
+
+	}
+
+	document.body.innerText = ''
+}
